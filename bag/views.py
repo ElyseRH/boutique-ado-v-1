1ceb30036c690d9ae594c8feefaf3bdcf2d7fa7d
ElyseRH <elyserhoran@gmail.com>
2022-06-06 11:59:18 +0000

    Add html and context for shopping bag

--- bag/views.py
+++ bag/views.py
@@ -1,24 +1,24 @@
+
 from django.shortcuts import render, redirect
 
 
 def view_bag(request):
-    """
-    Function view to return the shopping bag contents page.
-    """
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
 
-    quantity = int(request.POST.get('quantity'))  # will come from template as string so must convert
+    quantity = int(request.POST.get('quantity'))   # will come from template as string so must convert
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})  # looks for session and initializes to empty dict if doesn't
     # now that we have dict, can put product in it
+
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
-    
-    request.session['bag'] = bag  # this overwrites variable with the updated version
-    print(request.session['bag'])
+
+    request.session['bag'] = bag # this overwrites variable with the updated version
     return redirect(redirect_url)
