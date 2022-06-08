3dd4937f2c0fbd807a5c82dfacd0f7aa65419c96
ElyseRH <elyserhoran@gmail.com>
2022-06-08 11:56:26 +0000

    Add initial stripe code

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L8MWXCIsevnQGGYTmNITmMVgeva9HsAIXxLPokBxSbWevtvfnlE82BlR846yYQhgr1Et5ZzG5xaYwMbCX2yDxjQ00PJMGRnCM',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
