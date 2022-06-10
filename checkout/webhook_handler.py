4806a62be277c4a246a8833443ab7184c173df7f
ElyseRH <elyserhoran@gmail.com>
2022-06-10 13:54:25 +0000

    Complete webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -52,6 +52,7 @@ class StripeWH_Handler:
                     full_name__iexact=shipping_details.name,
                     email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
+                    # needs address key
                     country__iexact=shipping_details.address.country,
                     postcode__iexact=shipping_details.address.postal_code,
                     town_or_city__iexact=shipping_details.address.city,
