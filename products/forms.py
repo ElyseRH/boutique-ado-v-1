1a98d03adbcec130e6b54c769170dab21f508dec
ElyseRH <elyserhoran@gmail.com>
2022-06-11 12:23:39 +0000

    Add products/forms.py

---
+++ products/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Product, Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init(*args, **kwargs)
+        # want categories to show under their friendly names, so get all and then
+        # create a list of tuples of the friendly names with their associated category ids.
+        # this is called list comprehension, basically just a for loop that adds items to a list
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
