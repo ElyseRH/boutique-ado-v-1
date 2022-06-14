cff3d1758214e864351db69e56982713a878dab6
ElyseRH <elyserhoran@gmail.com>
2022-06-14 14:31:00 +0000

    Css and html refactored for mobile

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -219,8 +219,8 @@ STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
 DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
